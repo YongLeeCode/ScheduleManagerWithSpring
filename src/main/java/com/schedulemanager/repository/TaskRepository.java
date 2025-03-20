91485caa68b30cacd7ef40b5764ceea1271c8a62
YongLeeCode <yonghwan311@gmail.com>
2025-03-20 20:59:54 +0900

    feat: Task 레포지토리 구현

--- src/main/java/com/schedulemanager/repository/TaskRepository.java
+++ src/main/java/com/schedulemanager/repository/TaskRepository.java
@@ -1,14 +1,21 @@
 package com.schedulemanager.repository;
 
+import com.schedulemanager.entity.Task;
+
+import java.util.List;
+
 /**
- * @packageName    : com.schedulemanager.repository
- * @fileName       : TaskRepository
- * @author         : yong
- * @date           : 3/20/25
- * @description    :
+ * @author : yong
+ * @packageName : com.schedulemanager.repository
+ * @fileName : TaskRepository
+ * @date : 3/20/25
+ * @description :
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 3/20/25        yong       최초 생성
- */public interface TaskRepository {
+ */
+public interface TaskRepository {
+    void save(Task task);
+    List<Task> findAll();
 }
