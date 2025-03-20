afa0a39a3eae03fa0740de2405df72c2adb33afb
YongLeeCode <yonghwan311@gmail.com>
2025-03-20 20:58:48 +0900

    feat: Task 엔티티 구현

--- src/main/java/com/schedulemanager/entity/Task.java
+++ src/main/java/com/schedulemanager/entity/Task.java
@@ -1,14 +1,49 @@
 package com.schedulemanager.entity;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.sql.Timestamp;
+
 /**
- * @packageName    : com.schedulemanager.entity
- * @fileName       : Task
- * @author         : yong
- * @date           : 3/20/25
- * @description    :
+ * @author : yong
+ * @packageName : com.schedulemanager.entity
+ * @fileName : Task
+ * @date : 3/20/25
+ * @description :
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 3/20/25        yong       최초 생성
- */public class Task {
+ */
+
+@Getter
+public class Task {
+    private Long id;
+    private String title;
+    private String contents;
+    private String userName;
+    private String password;
+
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    public Task(String title, String contents, String userName, String password) {
+        this.title = title;
+        this.contents = contents;
+        this.userName = userName;
+        this.password = password;
+        this.createdAt = Timestamp.valueOf(LocalDateTime.now());
+        this.updatedAt = Timestamp.valueOf(LocalDateTime.now());
+    }
+
+    public Task(long id, String title, String contents, String userName, String password, Timestamp createdAt, Timestamp updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.contents = contents;
+        this.userName = userName;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
