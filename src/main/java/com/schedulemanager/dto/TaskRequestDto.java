b09c432e3781beea7cd127662c409d8ae61aa2c0
YongLeeCode <yonghwan311@gmail.com>
2025-03-20 20:58:17 +0900

    feat: Task의 요구,응답 dto 구현

--- src/main/java/com/schedulemanager/dto/TaskRequestDto.java
+++ src/main/java/com/schedulemanager/dto/TaskRequestDto.java
@@ -1,14 +1,27 @@
 package com.schedulemanager.dto;
 
+import lombok.Getter;
+
 /**
- * @packageName    : com.schedulemanager.dto
- * @fileName       : TaskRequestDto
- * @author         : yong
- * @date           : 3/20/25
- * @description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 3/20/25        yong       최초 생성
- */public class TaskRequestDto {
+ * @author : yong
+ * @packageName : com.schedulemanager.dto
+ * @fileName : TaskRequestDto
+ * @date : 3/20/25
+ * @description :
+
+ */
+
+@Getter
+public class TaskRequestDto {
+    private String title;
+    private String contents;
+    private String userName;
+    private String password;
+
+    public TaskRequestDto(String title, String contents, String userName, String password) {
+        this.title = title;
+        this.contents = contents;
+        this.userName = userName;
+        this.password = password;
+    }
 }
