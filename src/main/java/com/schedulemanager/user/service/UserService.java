088815e4df95f78120d486be7f8b9319ab9b8c11
YongLeeCode <yonghwan311@gmail.com>
2025-03-21 20:59:07 +0900

    feat: user 서비스 구현

--- src/main/java/com/schedulemanager/user/service/UserService.java
+++ src/main/java/com/schedulemanager/user/service/UserService.java
@@ -1,10 +1,21 @@
 package com.schedulemanager.user.service;
 
+import com.schedulemanager.user.dto.UserRequestDto;
+import com.schedulemanager.user.dto.UserResponseDto;
+
 /**
- * @packageName    : com.schedulemanager.user.service
- * @fileName       : UserService
- * @author         : yong
- * @date           : 3/21/25
- * @description    :
- */public interface UserService {
+ * @author : yong
+ * @packageName : com.schedulemanager.user.service
+ * @fileName : UserService
+ * @date : 3/21/25
+ * @description :
+ */
+public interface UserService {
+    void save(UserRequestDto dto);
+
+    UserResponseDto findUserById(long id);
+
+    void updateUser(long id, String password, UserRequestDto dto);
+
+    void deleteUser(long id, String password);
 }
