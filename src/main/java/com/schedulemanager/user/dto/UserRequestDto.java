26f79d2bffea34b5deadc55f09ccdb3d1e3e7412
YongLeeCode <yonghwan311@gmail.com>
2025-03-21 20:58:32 +0900

    feat: user dto 구현

--- src/main/java/com/schedulemanager/user/dto/UserRequestDto.java
+++ src/main/java/com/schedulemanager/user/dto/UserRequestDto.java
@@ -1,10 +1,19 @@
 package com.schedulemanager.user.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
- * @packageName    : com.schedulemanager.user.dto
- * @fileName       : UserRequestDto
- * @author         : yong
- * @date           : 3/21/25
- * @description    :
- */public class UserRequestDto {
+ * @author : yong
+ * @packageName : com.schedulemanager.user.dto
+ * @fileName : UserRequestDto
+ * @date : 3/21/25
+ * @description :
+ */
+@Getter
+@RequiredArgsConstructor
+public class UserRequestDto {
+    private final String name;
+    private final String email;
+    private final String password;
 }
--- src/main/java/com/schedulemanager/user/dto/UserResponseDto.java
+++ src/main/java/com/schedulemanager/user/dto/UserResponseDto.java
@@ -1,10 +1,33 @@
 package com.schedulemanager.user.dto;
 
+import com.schedulemanager.user.entity.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.Timestamp;
+
 /**
- * @packageName    : com.schedulemanager.user.dto
- * @fileName       : UserResponseDto
- * @author         : yong
- * @date           : 3/21/25
- * @description    :
- */public class UserResponseDto {
+ * @author : yong
+ * @packageName : com.schedulemanager.user.dto
+ * @fileName : UserResponseDto
+ * @date : 3/21/25
+ * @description :
+ */
+@Getter
+public class UserResponseDto {
+    private final long id;
+    private final String name;
+    private final String email;
+    private final String password;
+    private final Timestamp createdAt;
+    private final Timestamp updatedAt;
+
+    public UserResponseDto(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.createdAt = user.getCreatedAt();
+        this.updatedAt = user.getUpdatedAt();
+    }
 }
