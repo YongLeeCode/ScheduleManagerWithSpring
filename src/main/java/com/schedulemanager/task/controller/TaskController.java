b09b0deb5045b04edee280f0ee2fc0cd057a027a
YongLeeCode <yonghwan311@gmail.com>
2025-03-24 13:09:17 +0900

    feat: paging param 추가

--- src/main/java/com/schedulemanager/task/controller/TaskController.java
+++ src/main/java/com/schedulemanager/task/controller/TaskController.java
@@ -33,8 +33,8 @@ public class TaskController {
     }
 
     @GetMapping
-    public List<TaskResponseDto> findAll() {
-        return taskService.findAllTasks();
+    public List<TaskResponseDto> findAll(@RequestParam int page) {
+        return taskService.findAllTasks(page);
     }
 
     @GetMapping("/{id}")
