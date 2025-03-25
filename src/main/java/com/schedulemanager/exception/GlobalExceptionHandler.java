ff6c95a93d4f819f2bb6cc5ec3c6ea78783fba73
YongLeeCode <yonghwan311@gmail.com>
2025-03-25 15:06:12 +0900

    feat: 커스텀 예외처리 클래스 구현

---
+++ src/main/java/com/schedulemanager/exception/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.schedulemanager.exception;
+
+import com.schedulemanager.user.dto.UserResponseDto;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestValueException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author : yong
+ * @packageName : com.schedulemanager.exception
+ * @fileName : ExceptionHandler
+ * @date : 3/25/25
+ * @description :
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handleNullPointerException(NullPointerException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRunTimeException(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("회원 탈퇴를 원하시면 작성한 일정을 모두 지워주세요.");
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<String> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이 요청은 " + e.getParameterName() + " 파라미터를 포함하셔야합니다.");
+    }
+
+
+}
